baf97d4c3e981adc18477febb38ed98b4da586dc
Vladislav <vladislav.lants@hmail.com>
2017-04-05 01:07:13 +0400

    add studorg add course

--- bot.py
+++ bot.py
@@ -1,45 +1,91 @@
 import telebot
 from telebot import types
+import configparser
+
+config = configparser.ConfigParser()
+config.read("config.ini",encoding = 'UTF-8')
 
 
-bot = telebot.TeleBot("370462510:AAElsbNsJwqO9P0Ucuv6XHJQc7AH-5Xjw68")
+bot = telebot.TeleBot(config["DEFAULT"]["Token"])
 
 
 @bot.message_handler(commands=['start', 'help'])
 def start_message(message):
-    bot.send_message(message.chat.id,
-                     "\nХочешь стать крутым IT-специалистом или cвязистом? Тогда тебе к нам!")
-    bot.send_message(message.chat.id, "Я помогу тебе узнать немного больше о нашем ВУЗе! Воспользуйся простыми "
-                                      "командами чтобы узнать интересующую тебя информацию!\n"
-                                      "/course - Наши курсы для абитуриентов\n"
-                                      "/specialties - Направления подготовки\n"
-                                      "/guide - Руководство по поступлению\n"
-                                      "/rating - Узнай свой рейтинг\n"
-                                      "/contacts - Контактая информация\n"
-                                      "/calculator - Подсчитай свои баллы и узнай на какую спецальноть ты проходишь\n"
-                                      "/studorganizations - внеучебная деятельность\n"
-                                      "/about - О разработчитах\n"
-                                      "/excursion - интерактивная экскурсия по университету")
+    bot.send_message(message.chat.id, 'Хочешь стать крутым IT-специалистом или cвязистом? Тогда тебе к нам!')
+    bot.send_message(message.chat.id, 'Я помогу тебе узнать немного больше о нашем ВУЗе! Воспользуйся простыми '
+                                      'командами чтобы узнать интересующую тебя информацию!\n'
+                                      '/course - Курсы для абитуриентов\n'
+                                      '/specialties - Направления подготовки\n'
+                                      '/guide - Руководство по поступлению\n'
+                                      '/rating - Узнай свой рейтинг\n'
+                                      '/contacts - Контактая информация\n'
+                                      '/calculator - Подсчитай свои баллы и узнай на какую спецальность ты проходишь\n'
+                                      '/studorganizations - внеучебная деятельность\n'
+                                      '/about - О разработчитах\n'
+                                      '/excursion - интерактивная экскурсия по университету')
 
 
 @bot.message_handler(commands=['course'])
 def course(message):
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True,one_time_keyboard=True)
-    keyboard.row('Подготовка к ЕГЭ и внутренним экзаменам','Школа программистов')
-    keyboard.row('Инженерный лицей','Контактная информация')
-    msg = bot.send_message(message.chat.id, "Тест!",reply_markup=keyboard)
+    keyboard.row('Инженерный лицей','Школа программистов')
+    keyboard.row('Подготовка к ЕГЭ и внутренним экзаменам')
+    msg = bot.send_message(message.chat.id, 'Выберите интересующий вас курс!',reply_markup=keyboard)
     bot.register_next_step_handler(msg, process_step)
 
 def process_step(message):
     if message.text == 'Подготовка к ЕГЭ и внутренним экзаменам':
         markup = types.ReplyKeyboardRemove(selective=False)
-        bot.send_message(message.chat.id, 'К ЕГЭ!', reply_markup=markup)
+        bot.send_message(message.chat.id, text=config["COURSE"]["ege"], parse_mode='HTML',reply_markup=markup)
+        #link = types.InlineKeyboardMarkup()
+        #url_button = types.InlineKeyboardButton(text="Записаться на курсы!",
+                                                #url="http://abitur.psuti.ru/center/podgotovitelnye-kursy-k-ege-i-vnutrennim-ekzamenam/")
+        #link.add(url_button)
+        #bot.send_message(message.chat.id, "Получить более подробную информаци, а также записаться на курсы,"
+                                          #" можно перейдя по кнопке.",reply_markup=link)
+        bot.send_message(message.chat.id, 'олучить более подробную информаци, а также записаться на курсы,'
+                                          ' можно перейдя по ссылке: http://abitur.psuti.ru/center/podgotovitelnye-kursy-k-ege-i-vnutrennim-ekzamenam/', disable_web_page_preview=True)
+    elif message.text == 'Школа программистов':
+        markup = types.ReplyKeyboardRemove(selective=False)
+        bot.send_message(message.chat.id, '<b>Школа программистов</b>\n'
+                                          'Обучение платное для учащихся 8-х, 9-х, 10-х и 11-х классов, проводится в 3 этапа:\n'
+                                          '1. Информатика;\n'
+                                          '2. Программирование;\n'
+                                          '3. Информационные системы и технологии.\n'
+                                          'Срок обучения в школе – 5 месяцев.\n'
+                                          'Окончившим Школу программистов выдается «Свидетельство об окончании», которое дает право преимущественного зачисления в ПГУТИ.\n'
+                                          'Справки по телефону: (846) 228-00-51,  (846) 228-00-58.', reply_markup=markup, parse_mode='HTML')
+        bot.send_message(message.chat.id, 'Получить более подробную информаци, а также записаться в школу программистов,'
+                                          ' можно перейдя по ссылке: http://abitur.psuti.ru/center/shkola-programmistov/', disable_web_page_preview=True, parse_mode='HTML')
+    elif message.text == 'Инженерный лицей':
+        markup = types.ReplyKeyboardRemove(selective=False)
+        bot.send_message(message.chat.id, text=config["COURSE"]["engineer"], reply_markup=markup, parse_mode='HTML')
+        bot.send_message(message.chat.id, 'Получить более подробную информаци, а также записаться на занятия,'
+                                          ' можно перейдя по ссылке: http://abitur.psuti.ru/center/inzhenernyy-litsey/',
+                         disable_web_page_preview=True, parse_mode='HTML')
+    else:
+        markup = types.ReplyKeyboardRemove(selective=False)
+        bot.send_message(message.chat.id, 'Я Вас не понял, попробуйте еще раз /course '
+                                          'или воспользуйтесь /help для справки.', reply_markup=markup)
+
 
 
 @bot.message_handler(commands=['specialties'])
 def specialties(message):
-    bot.send_message(message.chat.id, "Тест!")
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True,one_time_keyboard=True)
+    keyboard.add('Академический бакалавриат')
+    keyboard.add('Прикладной бакалавриат')
+    keyboard.add('Специалитет')
+    msg = bot.send_message(message.chat.id, 'Выберите необходимую степень!',reply_markup=keyboard)
+    bot.register_next_step_handler(msg, process_step)
 
+def process_step(message):
+    if message.text == 'Академический бакалавриат':
+        bot.send_message(message.chat.id, 'Академический бакалавриат')
+    elif message.text == 'Прикладной бакалавриат':
+        bot.send_message(message.chat.id, 'Прикладной бакалавриат')
+    elif message.text == 'Специалитет':
+        bot.send_message(message.chat.id, 'Специалитет')
 
 @bot.message_handler(commands=['guide'])
 def guide(message):
@@ -53,7 +99,18 @@ def rating(message):
 
 @bot.message_handler(commands=['contacts'])
 def contacts(message):
-    bot.send_message(message.chat.id, "Тест!")
+    bot.send_message(message.chat.id, text='<b>Контактная информация</b>\n\n'
+                                           '<b>Телефоны:</b> '
+                                           '(846)228-00-51, '
+                                           '(846)228-00-58\n'
+                                           '<b>Email:</b> '
+                                           'cdou@psati.ru\n'
+                                           '<b>Проезд:</b> '
+                                           'остановка "Телецентр"\n'
+                                           '• автобусами (1, 23, 30, 37, 47, 56),\n'
+                                           '• троллейбусами (2, 4, 12, 17, 19),\n'
+                                           '• маршрутными такси (1, 1к, 4 (216), 21м, 23, 37, 47, 67, 91, 110, 257д, 259, 269, 373, 392, 392а, 410а, 492).', parse_mode='HTML')
+    bot.send_venue(message.chat.id, 53.2256561, 50.1949533, 'Физический адрес:', '443090, г. Самара, ул.Московское шоссе, 77, комн.201')
 
 
 @bot.message_handler(commands=['calculator'])
@@ -78,64 +135,22 @@ def inline(c):
         vk_button = types.InlineKeyboardButton(text="Вконтакте", url="https://vk.com/itclub_psuti")
         inst_button = types.InlineKeyboardButton(text="Instagram", url="https://www.instagram.com/itclub_psuti/")
         keyboard.add(vk_button,inst_button)
-        bot.send_message(c.message.chat.id, text='<b>Студенческий ИТ-клуб Поволжского Государственного'
-                                                 ' Университета Телекоммуникаций и Информатики</b>\n\n'
-                                                 'Основными направлениями деятельности клуба являются:\n'
-                                                 '• Научно–исследовательская работа студентов;\n'
-                                                 '• Организация мероприятий;\n'
-                                                 '• Работа со школьниками и абитуриентами;\n'
-                                                 '• Студенческие мастерские\n\n'
-                                                 'На счету студенческого ИТ–клуба сотрудничество с ведущими IT-компаниями '
-                                                 'области и региона.\n\n'
-                                                 'Члены клуба успешно представляют университет на разных уровнях. Они '
-                                                 'являются участниками, призерами и победителями Межвузовских, Областных и '
-                                                 'Международных Олимпиад и Конференций.\n\n'
-                                                 'Представители студенческого ИТ­ Клуба «IN-­IT with ПГУТИ» с удовольствием '
-                                                 'делятся своими навыками и всегда готовы к обмену опытом. Благодаря этому, '
-                                                 'при клубе существуют мастерские, организованные студентами по актуальным '
-                                                 'ИТ направлениям. ИТ–Клуб дает студентам возможность проявить себя в самых '
-                                                 'различных направлениях,что немаловажно для будущей профессиональной '
-                                                 'карьеры. Умение работать в команде, возможность проявить себя, как '
-                                                 'начинающего ученого, творческий подход к сложным задачам, опыт организации'
-                                                 ' мероприятий в ВУЗе и за его пределами, возможность развить педагогический'
-                                                 ' талант – это неполный список качеств, которые студент может открыть в '
-                                                 'себе, работая совместно с ИТ–клубом.\n'
-                                                 'Вы всегда можете найти нас в социальных сетях! #itclub_psuti',
-                         parse_mode='HTML',reply_markup=keyboard)
+        bot.send_message(c.message.chat.id, text=config["ORG_INFORMATION"]["itclub"],parse_mode='HTML',reply_markup=keyboard)
         bot.send_photo(c.message.chat.id, photo="https://pp.userapi.com/c623223/v623223296/234e/vqj_1H5X-Bs.jpg")
     elif c.data == 'Профком':
         keyboard = types.InlineKeyboardMarkup()
         vk_button = types.InlineKeyboardButton(text="Вконтакте", url="https://vk.com/profkom_psuti")
         inst_button = types.InlineKeyboardButton(text="Instagram", url="https://www.instagram.com/profkompguti/")
         keyboard.add(vk_button, inst_button)
-        bot.send_message(c.message.chat.id, '<b>Студенческий профсоюз | ПГУТИ</b> – это явление сравнительно молодое, но уже '
-                                            'сейчас сумевшее объединить в себе большинство обучающихся в университете.\n\n '
-                                            'Это организация, которой студент не безразличен, где он — личность, '
-                                            'которая стремится наполнить событиями окружающую действительность, сделать'
-                                            ' её более интересной. Он существует для того, чтобы объединить свои силы и'
-                                            ' способности, чтобы сделать студенческую жизнь по-настоящему интересной и'
-                                            ' увлекательной.\n\n '
-                                            'Этот мощный механизм, запущенный с помощью администрации ПГУТИ, сумел '
-                                            'вовлечь в свою работу самых активных, талантливых студентов. Преодолевая '
-                                            'определённые трудности, наша организация достигает поставленных целей: '
-                                            'проведение различных творческих вечеров, конференций, спортивных '
-                                            'мероприятий. Всё это вряд ли стало бы возможным без сильной поддержки '
-                                            'администрации ПГУТИ, которая идёт навстречу организаторам, воспитателям, '
-                                            'одним словом, всем тем, кто работает на благо студентов.\n\n'
-                                            'Основными сферами деятельности профсоюза являются постановка и реализация'
-                                            ' самых различных задач социального характера, спортивно-оздоровительные '
-                                            'мероприятия. Каждый проект обсуждается и согласуется, в результате этого '
-                                            'каждый может внести свои изменения, сделать замечания. Открытость, '
-                                            'возможность влиять на события студенческой жизни в университете – именно '
-                                            'по такому принципу организует свою деятельность профсоюз ПГУТИ. И этому '
-                                            'способствует сотрудничество с СМЦ, студ.городком, редакцией газеты '
-                                            '«Академия связи». Из её публикаций и статей можно почерпнуть сведения о '
-                                            'деятельности профсоюза. По заранее утверждённой комплексной программе '
-                                            'работы профком проводит различные мероприятия.',
-                         parse_mode='HTML',reply_markup=keyboard)
+        bot.send_message(c.message.chat.id, text=config["ORG_INFORMATION"]["profkom"],parse_mode='HTML',reply_markup=keyboard)
         bot.send_photo(c.message.chat.id, photo="https://pp.userapi.com/c11086/v11086252/59/ALr9Fe6aJX4.jpg")
     elif c.data == 'СМЦ':
-        bot.send_message(c.message.chat.id, 'Песни-пляски')
+        keyboard = types.InlineKeyboardMarkup()
+        vk_button = types.InlineKeyboardButton(text="Вконтакте", url="https://vk.com/smcpsuti")
+        keyboard.add(vk_button)
+        bot.send_message(c.message.chat.id, text=config["ORG_INFORMATION"]["smc"], parse_mode='HTML',
+                         reply_markup=keyboard)
+        bot.send_photo(c.message.chat.id, photo="https://pp.userapi.com/c624423/v624423730/46ffb/o64s1rt7Sds.jpg")
     elif c.data == 'РСО':
         bot.send_message(c.message.chat.id, 'Разнорабочие')
     elif c.data == 'Спортивные секции':
